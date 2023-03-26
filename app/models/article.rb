91008a28ef01263357f8149c0230f32361d43ef5
Cansu Zor <cansuzor@Cansus-Air.home>
2023-03-26 19:04:18 -0400

    new CRUD operation added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true
+    #title must be present 
+    validates :body, presence: true, length: {minimum: 10}
+    #body must be minimum 10 characters long
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
     #maps root route to index action of ArticlesController
 
 
-  get "/articles", to: "articles#index"
+  #get "/articles", to: "articles#index"
+  #get "articles/:id", to: "articles#show" replaced by resources:articles
+
+  resources :articles
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
