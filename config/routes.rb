315d3dc33137dd7381aa045115299d5cfa3dce86
Cansu Zor <cansuzor@Cansus-Air.lan>
2023-03-25 20:14:53 -0400

    database migrations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  
+  root "articles#index" 
+    #maps root route to index action of ArticlesController
+
+
   get "/articles", to: "articles#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
