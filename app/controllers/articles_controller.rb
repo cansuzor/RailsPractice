7f9a9aa992840934f509efa75e8131937754f813
Cansu Zor <cansuzor@Cansus-Air.lan>
2023-03-27 17:08:02 -0400

    controller/view edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  # 
+  # GET /articles
   def index
     @articles = Article.all
   end
@@ -8,16 +10,23 @@ class ArticlesController < ApplicationController
   end
 
   def new 
+    # 1. Grab the data you need for your view here
     @article = Article.new
+    # 2. Then render the view/html (using the data)
+    # By default rails renders this view
+    # render 'articles/new.html.erb'
   end
 
+  # POST /articles
   def create
+    # Creating article in memory with the data sent
     @article = Article.new(article_params)
-      if @article.save
-        redirect_to @article
-      else 
-        render :new,status: :unprocessable_entity
-      end
+    # Then we need to save the data to our DB
+    if @article.save
+      redirect_to article_path(@article) # /articles/:id
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 
 private
